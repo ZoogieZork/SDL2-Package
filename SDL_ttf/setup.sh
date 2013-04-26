50347594ff7de565236cb53b733f8d0c52b0a4b1
Michael Imamura <zoogie@lugatgt.org>
2013-04-25 20:38:59 -0400

    Refactor build tasks into functions.sh.

--- SDL_ttf/setup.sh
+++ SDL_ttf/setup.sh
@@ -6,45 +6,13 @@
 #
 # Licensed under the terms of the MIT License.  See LICENSE.txt.
 
+source "$(dirname "$0")/../functions.sh"
+
 DATADIR="$(dirname "$0")"
 PROJECT="$1"
 
-if [[ "$PROJECT" == "" ]]; then
-	echo "Usage: $0 projectdir" >&2
-	exit 1
-fi
-if [[ ! -d "$PROJECT" ]]; then
-	echo "Project is not a directory: $PROJECT" >&2
-	exit 1
-fi
-
-echo "==> Prepending changelog."
-CLTARGET="$PROJECT/debian/changelog"
-CLDIST="$CLTARGET.dist"
-CLPRE="$DATADIR/changelog-pre.txt"
-needscl=1
-if [[ -f "$CLDIST" ]]; then
-	# Check if the changelog has already been modified.
-	lines=$(wc -l "$CLPRE" | cut -d ' ' -f 1)
-	targetmd5=$(head -n "$lines" "$CLTARGET" | md5sum | cut -d ' ' -f 1)
-	premd5=$(md5sum "$CLPRE" | cut -d ' ' -f 1)
-	if [[ "$targetmd5" = "$premd5" ]]; then
-		needscl=0
-	fi
-fi
-if [[ $needscl == 1 ]]; then
-	echo "--> Updating changelog from $CLPRE"
-	cp "$CLTARGET" "$CLDIST"
-	cat "$CLPRE" "$CLDIST" > "$CLTARGET"
-else
-	echo "--> Changelog up-to-date."
-fi
-
-echo "==> Applying patches."
-PATCHFILE="$DATADIR/depends.patch"
-echo "--> $PATCHFILE"
-( cd "$PROJECT" ; patch -tN -p1 ) < "$PATCHFILE"
-
-echo "==> Building package."
-( cd "$PROJECT" ; debuild -i -S )
+verify_project "$PROJECT"
+prepend_changelog "$PROJECT" "$DATADIR"
+apply_patches "$PROJECT" "$DATADIR/depends.patch"
+build_source_pkg "$PROJECT"
 
---
+++ functions.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# functions.sh
+#   Common bits used in SDL2 package build scripts.
+#   Copyright (C) 2013 Michael Imamura.
+#
+# Licensed under the terms of the MIT License.  See LICENSE.txt.
+
+function verify_project {
+	PROJECT="$1"
+
+	if [[ "$PROJECT" == "" ]]; then
+		echo "Usage: $0 projectdir" >&2
+		exit 1
+	fi
+	if [[ ! -d "$PROJECT" ]]; then
+		echo "Project is not a directory: $PROJECT" >&2
+		exit 1
+	fi
+}
+
+function prepend_changelog {
+	PROJECT="$1"
+	DATADIR="$2"
+	
+	echo "==> Prepending changelog."
+	CLTARGET="$PROJECT/debian/changelog"
+	CLDIST="$CLTARGET.dist"
+	CLPRE="$DATADIR/changelog-pre.txt"
+	needscl=1
+	if [[ -f "$CLDIST" ]]; then
+		# Check if the changelog has already been modified.
+		lines=$(wc -l "$CLPRE" | cut -d ' ' -f 1)
+		targetmd5=$(head -n "$lines" "$CLTARGET" | md5sum | cut -d ' ' -f 1)
+		premd5=$(md5sum "$CLPRE" | cut -d ' ' -f 1)
+		if [[ "$targetmd5" = "$premd5" ]]; then
+			needscl=0
+		fi
+	fi
+	if [[ $needscl == 1 ]]; then
+		echo "--> Updating changelog from $CLPRE"
+		cp "$CLTARGET" "$CLDIST"
+		cat "$CLPRE" "$CLDIST" > "$CLTARGET"
+	else
+		echo "--> Changelog up-to-date."
+	fi
+}
+
+function _apply_patch {
+	PROJECT="$1"
+	PATCHFILE="$2"
+
+	echo "--> $PATCHFILE"
+	( cd "$PROJECT" ; patch -tN -p1 ) < "$PATCHFILE"
+}
+
+function apply_patches {
+	PROJECT="$1"
+	shift
+
+	echo "==> Applying patches."
+	for patchfile in "$@"; do
+		_apply_patch "$PROJECT" "$patchfile"
+	done
+}
+
+function build_source_pkg {
+	PROJECT="$1"
+
+	echo "==> Building source package."
+	( cd "$PROJECT" ; debuild -i -S )
+}
+
