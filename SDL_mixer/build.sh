f115c0e4a20bcef1b56373109729d374128c4c1c
Michael Imamura <zoogie@lugatgt.org>
2013-04-25 21:49:45 -0400

    Add SDL_mixer.

---
+++ SDL_mixer/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# build.sh
+#   Modify the package info and build new packages for SDL_mixer.
+#   Copyright (C) 2013 Michael Imamura.
+#
+# Licensed under the terms of the MIT License.  See LICENSE.txt.
+
+source "$(dirname "$0")/../functions.sh"
+
+DATADIR="$(dirname "$0")"
+PROJECT="${1:-SDL_mixer}"
+
+verify_project "$PROJECT"
+prepend_changelog "$PROJECT" "$DATADIR"
+apply_patches "$PROJECT" "$DATADIR/depends.patch"
+build_source_pkg "$PROJECT"
+
---
+++ SDL_mixer/fetch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# fetch.sh
+#   Download and extract the sources.
+#   Copyright (C) 2013 Michael Imamura.
+#
+# Licensed under the terms of the MIT license.  See LICENSE.txt.
+
+source "$(dirname "$0")/../functions.sh"
+
+fetch_hg 'http://hg.libsdl.org/SDL_mixer/' SDL_mixer
+
